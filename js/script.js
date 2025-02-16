99f8aad777fc7adc1711a6f9ed01d56b06108702
Conor McDonald <cm@mcdonaldconor.com>
2025-02-16 16:24:19 +0000

    menu fix

--- js/script.js
+++ js/script.js
@@ -112,4 +112,12 @@ document.addEventListener('DOMContentLoaded', function() {
           navbar.classList.remove('scrolled');
       }
   });
+
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('.nav-menu');
+
+  hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+      hamburger.classList.toggle('active');
+  });
 });
